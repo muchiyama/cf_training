a7c17a350bc80b6457e0608c9ab1fea1d002cbed
muchiyama <masatomo.uchiyama@jbs.com>
2020-02-04 18:23:27 +0900

    しりょ一旦つくった

--- front/src/global.js
+++ front/src/global.js
@@ -1,23 +1,21 @@
 
 
-function decodeBytesFromBase64(base64) {
-    return atob(base64.replace(/^.*,/, ''));
+function decodeBytesFromBase64(b64) {
+    return atob(b64.replace(/^.*,/, ''));
 }
 
-function convertbytesToBinary(bytes) {
-    var buffer = new Uint8Array(bytes.length);
-    for (var i = 0;i < bytes.length;i++) {
-        buffer[i] = bytes.charCodeAt(i);
+function convertbytesToBinary(sb) {
+    let b = new Uint8Array(sb.length);
+    for (var i = 0;i < sb.length;i++) {
+        b[i] = sb.charCodeAt(i);
     }
-    return buffer;
+    return b;
 }
 
 function createBinaryFromBase64 (c) {
-    var buffer = convertbytesToBinary(decodeBytesFromBase64(c));
-
-    var blob = new Blob([buffer.buffer], {
+    let b = convertbytesToBinary(decodeBytesFromBase64(c));
+    let bb = new Blob([b.buffer], {
         type: 'audio/mp3'
     });
-
-    return window.URL.createObjectURL(blob);
+    return window.URL.createObjectURL(bb);
 }
